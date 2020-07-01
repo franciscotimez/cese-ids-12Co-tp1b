386743645f2cf568ad1032cdfaf742483ad3a673
Francisco Timez <franciscotimez@gmail.com>
2020-06-30 21:29:44 -0300

    Se agrega alumno Francisco timez

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s FRANCISCO_TIMEZ = {
+    .apellidos = "TIMEZ",
+    .nombres = "Francisco Gabriel",
+    .documento = "34.826.419",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &FRANCISCO_TIMEZ,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
